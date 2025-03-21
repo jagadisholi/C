028eab3174f6f749d1091854a89529c36eb98434
jagadisholi <olijagadish1@gamil.com>
2025-03-21 20:40:32 +0545

    OS

--- project.c
+++ project.c
@@ -1,137 +1,62 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
+[Project]
+FileName=Project.c
+Name=Project2
+Type=1
+Ver=2
+ObjFiles=
+Includes=
+Libs=
+PrivateResource=
+ResourceIncludes=
+MakeIncludes=
+Compiler=
+CppCompiler=
+Linker=
+IsCpp=0
+Icon=
+ExeOutput=
+ObjectOutput=
+LogOutput=
+LogOutputEnabled=0
+OverrideOutput=0
+OverrideOutputName=
+HostApplication=
+UseCustomMakefile=0
+CustomMakefile=
+CommandLine=
+Folders=
+IncludeVersionInfo=0
+SupportXPThemes=0
+CompilerSet=0
+CompilerSettings=0000000000000000000000000
+UnitCount=1
 
-#define MAX_STUDENTS 100
+[VersionInfo]
+Major=1
+Minor=0
+Release=0
+Build=0
+LanguageID=1033
+CharsetID=1252
+CompanyName=
+FileVersion=
+FileDescription=Developed using the Dev-C++ IDE
+InternalName=
+LegalCopyright=
+LegalTrademarks=
+OriginalFilename=
+ProductName=
+ProductVersion=
+AutoIncBuildNr=0
+SyncProduct=1
 
-struct Student {
-    char rollNumber[20];
-    char name[50];
-    char address[100];
-    char contact[20];
-};
-
-void addStudent(struct Student students[], int *numStudents) {
-    struct Student newStudent;
-    printf("Enter Roll Number: ");
-    scanf("%s", newStudent.rollNumber);
-    
-    printf("Enter Name: ");
-    scanf(" %[^\n]", newStudent.name);
-    
-    printf("Enter Address: ");
-    scanf(" %[^\n]", newStudent.address);
-    
-    printf("Enter Contact Number: ");
-    scanf("%s", newStudent.contact);
-    
-    students[*numStudents] = newStudent;
-    (*numStudents)++;
-    
-    printf("Student added successfully!\n");
-}
-
-void updateStudent(struct Student students[], int numStudents) {
-    char rollNumber[20];
-    int i;
-    
-    printf("Enter Roll Number of the student to update: ");
-    scanf("%s", rollNumber);
-    
-    for (i = 0; i < numStudents; i++) {
-        if (strcmp(students[i].rollNumber, rollNumber) == 0) {
-            printf("Enter Updated Address: ");
-            scanf(" %[^\n]", students[i].address);
-            
-            printf("Enter Updated Contact Number: ");
-            scanf("%s", students[i].contact);
-            
-            printf("Student information updated successfully!\n");
-            return;
-        }
-    }
-    
-    printf("Student with Roll Number %s not found!\n", rollNumber);
-}
-
-void searchStudent(struct Student students[], int numStudents) {
-    char rollNumber[20];
-    int i;
-    
-    printf("Enter Roll Number of the student to search: ");
-    scanf("%s", rollNumber);
-    
-    for (i = 0; i < numStudents; i++) {
-        if (strcmp(students[i].rollNumber, rollNumber) == 0) {
-            printf("Student Found!\n");
-            printf("Roll Number: %s\n", students[i].rollNumber);
-            printf("Name: %s\n", students[i].name);
-            printf("Address: %s\n", students[i].address);
-            printf("Contact Number: %s\n", students[i].contact);
-            return;
-        }
-    }
-    
-    printf("Student with Roll Number %s not found!\n", rollNumber);
-}
-
-void displayAllStudents(struct Student students[], int numStudents) {
-    int i;
-    
-    printf("Student Records:\n");
-    
-    for (i = 0; i < numStudents; i++) {
-        printf("Roll Number: %s\n", students[i].rollNumber);
-        printf("Name: %s\n", students[i].name);
-        printf("Address: %s\n", students[i].address);
-        printf("Contact Number: %s\n", students[i].contact);
-        printf("----------------------\n");
-    }
-    
-    if (numStudents == 0) {
-        printf("No students found!\n");
-    }
-}
-
-int main() {
-    struct Student students[MAX_STUDENTS];
-    int numStudents = 0;
-    char choice;
-    
-    do {
-        printf("\n..... welcome to student record management system....\n");
-        printf("1. Add Student\n");
-        printf("2. Update Student\n");
-        printf("3. Search Student\n");
-        printf("4. Display All Students\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf(" %c", &choice);
-        
-        switch (choice) {
-            case '1':
-                addStudent(students, &numStudents);
-                break;
-            case '2':
-                updateStudent(students, numStudents);
-                break;
-            case '3':
-                searchStudent(students, numStudents);
-                break;
-            case '4':
-                displayAllStudents(students, numStudents);
-                break;
-            case '5':
-                printf("Exiting the program...\n");
-                break;
-            default:
-                printf("Invalid choice! Please enter a valid option.\n");
-                break;
-        }
-    } while (choice != '5');
-
-    return 0;
-}
-
-            
+[Unit1]
+FileName=main.c
+CompileCpp=0
+Folder=
+Compile=1
+Link=1
+Priority=1000
+OverrideBuildCmd=0
+BuildCmd=
 
